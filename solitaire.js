ceb89998529b3c3d74983729484a040921e28611
nikhilEmpiric <162993267+nikhilEmpiric@users.noreply.github.com>
2024-03-15 17:35:10 +0530

    add popup functionalities
    functionalities

--- solitaire.js
+++ solitaire.js
@@ -14,4 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
     hidePopupButton.addEventListener("click", function () {
         popup.style.display = "none";
     });
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the buttons and the popup
+    const card_face = document.getElementById("card_face");
+    const bgcard = document.getElementById("bgcard");
+    const cardface = document.getElementById("cardface");
+ 
+    // Show the popup when the "Show Popup" button is clicked
+    card_face.addEventListener("click", function () {
+        cardface.style.display = "absolute";
+        top ="0px";
+    });
+ 
+    // Hide the popup when the "Hide Popup" button is clicked
+    card_face.addEventListener("click", function () {
+        bgcard.style.display = "none";
+    });
+
 });
