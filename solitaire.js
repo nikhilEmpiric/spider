dfb37343ac20d65493c89dcce5facbdc4836e3b0
nikhilEmpiric <162993267+nikhilEmpiric@users.noreply.github.com>
2024-03-15 14:55:46 +0530

    game-main
    popup add

--- solitaire.js
+++ solitaire.js
@@ -1,14 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     // Get references to the buttons and the popup
-    const showPopupButton = document.getElementById("showPopup");
+    const showPopupButton = document.querySelector(".showPopup");
     const hidePopupButton = document.getElementById("hidePopup");
     const popup = document.getElementById("popup");
-
+ 
     // Show the popup when the "Show Popup" button is clicked
     showPopupButton.addEventListener("click", function () {
-        popup.style.display = "block";
+        popup.style.display = "flex";
     });
-
+ 
     // Hide the popup when the "Hide Popup" button is clicked
     hidePopupButton.addEventListener("click", function () {
         popup.style.display = "none";
