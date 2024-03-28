9c0d4dcf02f8a0e83de3e69bcbb20d85307705ff
nikhilEmpiric <162993267+nikhilEmpiric@users.noreply.github.com>
2024-03-28 17:21:42 +0530

    Solitaire js
    - Solitaire js soul this error

--- solitaire.js
+++ solitaire.js
@@ -1,80 +1,81 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     // Get references to the buttons and the popup
-    const showPopupButton = document.querySelector(".showPopup");
-    const hidePopupButton = document.getElementById("hidePopup");
-    const popup = document.getElementById("popup");
-    const cardface_box1 = document.querySelector(".cardface_box");
-    const cardback1 = document.querySelector(".cardback");
-    const background1= document.getElementById("background");
-    const card_face1= document.getElementById("card_face");
-    const card_back1= document.getElementById("card_back");
-    const bg = document.getElementById("bgcard");
+    let showPopupButton = document.querySelector(".showPopup");
+    let hidePopupButton = document.getElementById("hidePopup");
+    let popup = document.getElementById("popup");
+    let cardface_box1 = document.querySelector(".cardface_box");
+    let cardback1 = document.querySelector(".cardback");
+    let background1 = document.getElementById("background");
+    let card_face1 = document.getElementById("card_face");
+    let card_back1 = document.getElementById("card_back");
+    let bg = document.getElementById("bgcard");
 
-    // Show the popup when the "Show Popup" button is clicked
-    showPopupButton.addEventListener("click", function () {
-        popup.style.display = "flex";
-        cardface_box1.style.display = "none";
-        cardback1.style.display = "none"
-        background1.setAttribute('class', 'active'); 
-        card_back1.classList.remove('active');
-        card_face1.classList.remove('active');
-        bg.style.display = "";
+    // Show the popup when the "Show Popup" button is clicked.\
+
+    if (showPopupButton) {
+        showPopupButton.addEventListener("click", function () {
+            popup.style.display = "flex";
+            cardface_box1.style.display = "none";
+            cardback1.style.display = "none"
+            background1.setAttribute('class', 'active');
+            card_back1.classList.remove('active');
+            card_face1.classList.remove('active');
+            bg.style.display = "grid";
+            // console.log("15");
         });
- 
-    // Hide the popup when the "Hide Popup" button is clicked
-    hidePopupButton.addEventListener("click", function () {
-        popup.style.display = "none";
-    });
+        // Hide the popup when the "Hide Popup" button is clicked
+        hidePopupButton.addEventListener("click", function () {
+            popup.style.display = "none";
+        });
+    }
 });
-
 
 document.addEventListener("DOMContentLoaded", function () {
     // const cardface_box = document.querySelector("cardface_box");
-    const background1= document.getElementById("background");
-    const card_face1= document.getElementById("card_face");
-    const card_back1= document.getElementById("card_back");
+    const background1 = document.getElementById("background");
+    const card_face1 = document.getElementById("card_face");
+    const card_back1 = document.getElementById("card_back");
     const bg = document.getElementById("bgcard");
     const cardface_box1 = document.querySelector(".cardface_box");
     const cardback1 = document.querySelector(".cardback");
     const bcc_button1 = document.querySelector(".bcc_button");
 
+    if (card_face1) {
         // Hide the popup when the "Hide Popup" button is clicked
         card_face1.addEventListener("click", function () {
-            card_face1.setAttribute('class', 'active'); 
+            card_face1.setAttribute('class', 'active');
             background1.classList.remove('active');
             card_back1.classList.remove('active');
             console.log("runned...")
             bg.style.display = "none";
-            cardface_box1.style.display = "";
+            cardface_box1.style.display = "grid";
             cardback1.style.display = "none";
         });
         background1.addEventListener("click", function () {
-            background1.setAttribute('class', 'active'); 
+            background1.setAttribute('class', 'active');
             card_back1.classList.remove('active');
             card_face1.classList.remove('active');
             console.log("runned...")
-            bg.style.display = "";
+            bg.style.display = "grid";
             cardface_box1.style.display = "none";
             cardback1.style.display = "none";
         });
-        card_back1.addEventListener("click", function () { 
-            card_back1.setAttribute('class', 'active'); 
+        card_back1.addEventListener("click", function () {
+            card_back1.setAttribute('class', 'active');
             background1.classList.remove('active');
             card_face1.classList.remove('active');
             console.log("runned...")
-            cardback1.style.display = "";
+            cardback1.style.display = "grid";
             cardface_box1.style.display = "none";
             bg.style.display = "none";
-
         });
+    }
 });
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
     // Get references to the buttons and the popup
-    const showPopupButton = document.querySelector(".setting");
+    const showPopup = document.querySelector(".setting");
     const hidePopupButton = document.getElementById("setting_hidePopup");
     const setting_popup = document.getElementById("setting_popup");
     const daily = document.getElementById("daily");
@@ -82,55 +83,65 @@ document.addEventListener("DOMContentLoaded", function () {
     const days = document.getElementById("days");
     const event = document.getElementById("event");
 
-    // Show the popup when the "Show Popup" button is clicked
-    showPopupButton.addEventListener("click", function () {
-        setting_popup.style.display = "flex";
-        event.style.display = "none";
-        days.style.display = "";
-        daily.setAttribute('class', 'active'); 
-        events.classList.remove('active');
+    if (showPopup) {
+        // Show the popup when the "Show Popup" button is clicked
+        showPopup.addEventListener("click", function () {
+            setting_popup.style.display = "flex";
+            event.style.display = "none";
+            days.style.display = "";
+            daily.setAttribute('class', 'active');
+            events.classList.remove('active');
         });
- 
-    // Hide the popup when the "Hide Popup" button is clicked
-    hidePopupButton.addEventListener("click", function () {
-        setting_popup.style.display = "none";
-    });
-    daily.addEventListener("click", function () {
-        days.style.display = "";
-        event.style.display = "none";
-        daily.setAttribute('class', 'active'); 
-        events.classList.remove('active');
-    });
-    events.addEventListener("click", function () {
-        days.style.display = "none";
-        event.style.display = "";
-        daily.classList.remove('active');
-        events.setAttribute('class', 'active'); 
-    });
+
+        // Hide the popup when the "Hide Popup" button is clicked
+        hidePopupButton.addEventListener("click", function () {
+            setting_popup.style.display = "none";
+        });
+        daily.addEventListener("click", function () {
+            days.style.display = "";
+            event.style.display = "none";
+            daily.setAttribute('class', 'active');
+            events.classList.remove('active');
+        });
+        events.addEventListener("click", function () {
+            days.style.display = "none";
+            event.style.display = "";
+            daily.classList.remove('active');
+            events.setAttribute('class', 'active');
+        });
+    }
 });
 
 
 document.addEventListener("DOMContentLoaded", function () {
     // Get references to the buttons and the popup
-    const start_welcom = document.getElementById("start_welcome");
-    const welcom = document.getElementById("welcome");
-    const start_welcome_popup = document.querySelector(".start_welcome_popup");
-    const accept = document.getElementById("accept");
+    // let start_welcom = document.getElementById("start_welcome");
+    let welcom = document.getElementById("welcome");
+    let start_welcome_popup = document.querySelector(".start_welcome_popup");
+    let accept = document.getElementById("accept");
 
     // Show the popup when the "Show Popup" button is clicked
     // start_welcom.addEventListener("click", function () {
-        // start_welcome_popup.style.display = "none";
-        // cardface_box1.style.display = "none";
-        
-        // });
- 
-    // Hide the popup when the "Hide Popup" button is clicked
-    accept.addEventListener("click", function () {
-        start_welcome_popup.style.display = "none";
-        start_welcome.style.display = "block";
-    });
+    //     // start_welcome_popup.style.display = "none";
+    //     // cardface_box1.style.display = "none";
+    //     console.log("jbcy")
+    // });
 
+    // Hide the popup when the "Hide Popup" button is clicked
+    if (accept) {
+        accept.addEventListener("click", function () {
+            start_welcome_popup.style.display = "none";
+            start_welcome.style.display = "block";
+        });
+        welcom.addEventListener("click", function () {
+            // start_welcome_popup.style.display = "flex";
+        });
+    }
+    // accept.addEventListener("click", function () {
+    //     start_welcome_popup.style.display = "none";
+    //     start_welcome.style.display = "block";
+    // });
     // welcom.addEventListener("click", function () {
-        // start_welcome_popup.style.display = "flex";
+    //     // start_welcome_popup.style.display = "flex";
     // });
 });
